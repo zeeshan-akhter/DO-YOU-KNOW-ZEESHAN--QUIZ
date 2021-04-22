881078287532841742f068ea203a506e00829eeb
Zeeshan Akhter <zeeshanakhter957@gmail.com>
2021-04-22 17:23:52 +0530

    docs: added rules
    added pint rule to the game

---
+++ main.js
@@ -0,0 +1,50 @@
+
+var readlineSync = require("readline-sync");
+var score = 0;
+
+
+var userName = readlineSync.question("Enter your name: ");
+
+console.log("Welcome " + userName + "to DO YOU KNOW Zeeshan? ");
+console.log("Rules: You will be awarded 2 points for each correct answer");
+console.log("<------------------------------------------->");
+
+function play(question, answer){
+
+    var userAnswer = readlineSync.question(question);
+
+    if(userAnswer.toUpperCase() == answer.toUpperCase()){
+        console.log("You are right!");
+        score = score + 2;
+    }else{
+        console.log("You are wrong!");
+    }
+
+    console.log("Current score: ", score);
+    console.log("-----------------------");
+}
+
+var questions = [{
+    question: "What is my full name? ",
+    answer: "zeeshan akhter"
+},{
+    question: "My birth date? ",
+    answer: "28 "
+},{
+    question: "What is my age? ",
+    answer: "18"
+},{
+    question: "Where do I live? ",
+    answer: "gaya"
+},{
+    question: "My favourite fruit? ",
+    answer: "mango"
+}];
+
+for(var i=0; i<questions.length; i++){
+    var currentQuestion = questions[i];
+
+    play(currentQuestion.question, currentQuestion.answer)
+}
+
+console.log("YAY! You scored: ", score);
